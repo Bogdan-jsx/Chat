6b15e3d35400215a00719a74d9e12d54dd772c8f
Bogdan-jsx <sbogdan3755@gmail.com>
2021-08-26 22:37:15 +0300

    Added adaptive

--- backend/src/controllers/messagesController.js
+++ backend/src/controllers/messagesController.js
@@ -4,9 +4,7 @@ const messagesRepository = require("../db/repositories/messagesRepository");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    console.log("getting messages...");
     const messages = await messagesRepository.getAllMessages();
-    console.log(messages);
     res.json(messages);
 })
 
